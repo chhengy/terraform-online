0079b42b236d8f104be12e112c43d0fb8ec45dcd
LeavY <147359468+chhengy@users.noreply.github.com>
2023-11-30 10:47:59 +0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAWU43ATGIVNMM2J5M"
+  secret_key = "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"
+}
+
