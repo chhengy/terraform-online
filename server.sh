3abaf19dc4a4bf74d177a2e4b18765df30a2cc16
LeavY <147359468+chhengy@users.noreply.github.com>
2023-11-30 16:15:46 +0700

    Update server.sh

--- server.sh
+++ server.sh
@@ -2,6 +2,6 @@
 apt update
 apt upgrade -y
 apt install apache2 -y
-echo "<h1>Hello world from highly available group of ec2 instances</h1>" > /var/www/html/index.html
+echo "<h1>Hello World From Highly available Group of EC2 Instances</h1>" > /var/www/html/index.html
 systemctl start apache2
 systemctl enable apache2
